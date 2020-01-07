90b0186772c64e1802601be358b0a5e1716494ef
314pie <ian121363@gmail.com>
2020-01-07 18:04:49 +0800

    Add map view

--- CustomLoginDemo/View Controllers/HomeViewController.swift
+++ CustomLoginDemo/View Controllers/HomeViewController.swift
@@ -6,17 +6,74 @@
 //  Copyright © 2019 Christopher Ching. All rights reserved.
 //
 
+import MapKit
 import UIKit
+import CoreLocation
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeter:Double = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        checkLocationService()
         // Do any additional setup after loading the view.
     }
     
-
+    func setUpLocatinManager()  {
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLocation()  {
+        print("Centering view on user location")
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location,latitudinalMeters: regionInMeter,longitudinalMeters: regionInMeter)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationService()  {
+        if CLLocationManager.locationServicesEnabled(){
+            setUpLocatinManager()
+            checkLocationAuthentication()
+        }else{
+            
+        }
+    }
+    
+    func checkLocationAuthentication(){
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+           // mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            
+            if let location = locationManager.location?.coordinate{
+            print(location.latitude,"-",location.longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude:location.latitude,longitude: location.longitude)
+               mapView.addAnnotation(annotation)
+            }
+           
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        default:
+            break
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +84,17 @@ class HomeViewController: UIViewController {
     }
     */
 
+}
+
+extension HomeViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else{return}
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center,latitudinalMeters: regionInMeter,longitudinalMeters: regionInMeter)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthentication()
+    }
 }
