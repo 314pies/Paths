3695d12abb296709238b3c0272ad94ccd1e8afba
314pie <ian121363@gmail.com>
2020-01-08 01:49:10 +0800

    Update home page

--- CustomLoginDemo/View Controllers/HomeViewController.swift
+++ CustomLoginDemo/View Controllers/HomeViewController.swift
@@ -5,6 +5,8 @@ import Firebase
 
 class HomeViewController: UIViewController {
 
+    
+    
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
@@ -22,6 +24,17 @@ class HomeViewController: UIViewController {
             } else {
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
+                    
+                    let lat : Double? = document.data()["lat"] as? Double
+                    let lon : Double? = document.data()["long"] as? Double
+                    let imgPath : String? = document.data()["img"] as? String
+                    
+                    if lat != nil && lon != nil{
+                        let annotation = CustomPointAnnotation()
+                        annotation.imagePath = imgPath
+                        annotation.coordinate = CLLocationCoordinate2D(latitude:lat!,longitude: lon!)
+                        self.mapView.addAnnotation(annotation)
+                    }
                 }
             }
         }
@@ -54,14 +67,7 @@ class HomeViewController: UIViewController {
         case .authorizedWhenInUse:
            // mapView.showsUserLocation = true
             centerViewOnUserLocation()
-            
-            if let location = locationManager.location?.coordinate{
-            print(location.latitude,"-",location.longitude)
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = CLLocationCoordinate2D(latitude:location.latitude,longitude: location.longitude)
-               mapView.addAnnotation(annotation)
-            }
-           
+            self.mapView.delegate = self
             break
         case .denied:
             break
@@ -99,13 +105,54 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did update called")
         guard let location = locations.last else{return}
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegion.init(center: center,latitudinalMeters: regionInMeter,longitudinalMeters: regionInMeter)
         mapView.setRegion(region, animated: true)
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    func locationManager(manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthentication()
+    }
+}
+
+extension HomeViewController : MKMapViewDelegate{
+    class CustomPointAnnotation: MKPointAnnotation {
+        var imagePath: String!
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is CustomPointAnnotation) {
+            return nil
+        }
+
+        let reuseId = "test"
+
+        var anView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        if anView == nil {
+            anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            anView?.canShowCallout = true
+        }
+        else {
+            anView?.annotation = annotation as! CustomPointAnnotation
+        }
+
+        //Set annotation-specific properties **AFTER**
+        //the view is dequeued or created...
+
+        let cpa = annotation as! CustomPointAnnotation
+        print("cpa img: ",cpa.imagePath)
+       
+        let a =  UIImage(named:"StoryMaps")
+        anView?.image = Utilities.resizeImage(image: a!, targetSize: CGSize(width: 30,height: 30))
+        //anView?.image = UIImage(named:"StoryMaps")
+        
+        return anView
+    }
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        let cpa = view.annotation as! CustomPointAnnotation
+        print("cpa img2: ",cpa.imagePath)
     }
 }
