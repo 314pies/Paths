1dbbfcc2a39a1379dee752b4dd4d5e635a9829bd
314pie <ian121363@gmail.com>
2020-01-10 17:00:11 +0800

    Add working UIViewControllerRepresentable for switching between SwiftUI and storyboard

--- CustomLoginDemo/SwiftUI Views/ProfileView.swift
+++ CustomLoginDemo/SwiftUI Views/ProfileView.swift
@@ -9,13 +9,57 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var show = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        return Group {
+            if show {
+                HomeView()
+            }
+            else {
+               Button("play") {
+                    self.show = true
+                }
+//                .sheet(isPresented: $show) {
+//                    StoryView()
+//                }
+            }
+        }
+//       .sheet(item: true, content:HomeView())
     }
 }
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
+//struct ProfileView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProfileView()
+//    }
+//}
+
+struct HomeView : UIViewControllerRepresentable{
+    func makeUIViewController(context: UIViewControllerRepresentableContext<HomeView>) -> HomeViewController {
+         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController)
+        return controller as! HomeViewController
     }
+
+    func updateUIViewController(_ uiViewController: HomeViewController, context: UIViewControllerRepresentableContext<HomeView>) {
+        
+    }
+
+    typealias UIViewControllerType = HomeViewController
 }
+
+//struct StoryView :UIViewControllerRepresentable{
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<StoryView>) -> StoryWritterViewController {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//       let controller = storyboard.instantiateViewController(withIdentifier: Constants.Storyboard.storyWritterViewController)
+//        return controlle
+//    }
+//
+//    func updateUIViewController(_ uiViewController: StoryWritterViewController, context: UIViewControllerRepresentableContext<StoryView>) {
+//
+//    }
+//
+//    typealias UIViewControllerType = StoryWritterViewController
+//
+//}
