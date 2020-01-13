2b158f74a90f16dffd44a84476a2ef37d595179a
314pie <ian121363@gmail.com>
2020-01-13 23:03:08 +0800

    Intergrade Flickr API for viewing nearby photos from Flickr

--- CustomLoginDemo/FlickrAPI/FlickrSearch.swift
+++ CustomLoginDemo/FlickrAPI/FlickrSearch.swift
@@ -18,9 +18,10 @@ class FlickrSearch{
     
     //24.826990
     //121.013000
+    static let APIKey:String = "535a87a2f2c770de1777c7a7931a724f"
     static func fetchData(lat: String, lon: String) {
         print("Fetching Flicker data from location: ",lat," and ",lon)
-        if let url = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=535a87a2f2c770de1777c7a7931a724f&per_page=20&format=json&nojsoncallback=1&lat="+lat+"&lon="+lon) {
+        if let url = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+APIKey+"&per_page=20&format=json&nojsoncallback=1&lat="+lat+"&lon="+lon) {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 if let data = data, let searchData = try? JSONDecoder().decode(SearchData.self, from: data) {
                     photos = searchData.photos.photo
