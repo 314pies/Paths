74c41c89926d130a71a065768b148ea05435a82d
314pie <ian121363@gmail.com>
2020-01-14 00:46:49 +0800

    Add profile page. Add custom app icon

--- CustomLoginDemo/SwiftUI Views/ProfileView.swift
+++ CustomLoginDemo/SwiftUI Views/ProfileView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 
 struct ProfileView: View {
     @State private var show = false
+    
+    @State private var enableOfflineCache = false
+    @State private var name: String = "Ian"
+    // 2.
+    var Genders = ["Male", "Female", "Others"]
+    @State private var selectedMode = 0
     var body: some View {
         
         return Group {
@@ -17,9 +23,68 @@ struct ProfileView: View {
                 HomeView()
             }
             else {
-               Button("play") {
-                    self.show = true
-                }
+                 NavigationView {
+                    // 3.
+//                    Image("profileDefaultIcon").resizable()
+//                    .frame(width: 128.0, height: 128.0)
+//                    .clipShape(Circle())
+//                    .shadow(radius: 10)
+//                    Divider()
+//                    VStack(alignment: .leading) {
+//                        Text("Name:")
+//                        Text("Email:")
+//                        Text("Phone:")
+//                    }
+//                    Divider()
+//                    Button("back") {
+//                        self.show = true
+//                    }
+                          Form {
+                            HStack{
+                                Image("profileDefaultIcon")
+                                    .resizable()
+                                    .frame(width: 128.0, height: 128.0,alignment: .center)
+                                    .scaledToFill()
+                                    .clipShape(Circle())
+                                    .shadow(radius: 10)
+                            }
+                              // 4.
+                              Section(header: Text("Account")){
+                                
+                                HStack {
+                                    Text("User Name")
+                                    Spacer()
+//                                    TextField("Enter your name", text: $name)
+                                    Text("Ian Chen")
+                                }
+                                
+                                HStack {
+                                    Text("Email")
+                                    Spacer()
+                                    Text("ian121362@gmail.com")
+                                }
+                                Picker(selection: $selectedMode, label: Text("Gender")) {
+                                    ForEach(0..<Genders.count) {
+                                        Text(self.Genders[$0])
+                                    }
+                                }
+                              }
+                              
+                              // 5.
+                              Section(header: Text("Settings")) {
+                                Toggle(isOn: $enableOfflineCache) {
+                                      Text("Offline Cache")
+                                }
+                              }
+                          } .navigationBarTitle("Profiles & Settings")
+                            .navigationBarItems(trailing:
+                                Button("Done") {
+                                    self.show = true
+                                }
+                            )
+                            
+                        
+                      }
 //                .sheet(isPresented: $show) {
 //                    StoryView()
 //                }
@@ -29,11 +94,11 @@ struct ProfileView: View {
     }
 }
 
-//struct ProfileView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ProfileView()
-//    }
-//}
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
 
 struct HomeView : UIViewControllerRepresentable{
     func makeUIViewController(context: UIViewControllerRepresentableContext<HomeView>) -> HomeViewController {
